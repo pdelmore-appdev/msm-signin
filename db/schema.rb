e4aef57ef87e056eee328ecc2274bfdcaca4cdff
Paulina Delmore <delmore.paulina@gmail.com>
2023-03-06 22:10:43 +0000

    draft:resource director

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_06_220813) do
+ActiveRecord::Schema.define(version: 2023_03_06_221006) do
 
   create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.text "bio"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "directors", force: :cascade do |t|
     t.string "name"
     t.date "dob"
     t.text "bio"
