088e3f0409ecc23d6d69b3e9049817b83b0778e0
Paulina Delmore <delmore.paulina@gmail.com>
2023-03-07 19:04:17 +0000

    add navbar, start on validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -17,5 +17,9 @@ class Movie < ApplicationRecord
   belongs_to :director
   has_many :characters
 
-  has_many :cast, :through => :characters, :scource => :actor
+  has_many :cast, :through => :characters, :source => :actor
+
+
+
+  validates(:title, { :presence => true })
 end
